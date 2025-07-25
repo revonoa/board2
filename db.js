d45126d439c00ddcfe1dfc30a7015faaf594f7cc
revonoa <w2452510@gmail.com>
2025-07-25 09:10:21 +0900

    fix db

--- db.js
+++ db.js
@@ -2,8 +2,8 @@
 const mysql = require('mysql2');
 
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: '59.22.64.219',
+  user: 'chan',
   password: '641325',
   database: 'board'
 });
